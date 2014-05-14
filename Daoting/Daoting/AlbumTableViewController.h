fd80e3fbcd6615656c31f7b2000753fd9f133955
Kevin Zhao <kevin8093@126.com>
2014-05-14 22:50:37 +0800

    update

---
+++ Daoting/Daoting/AlbumTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  AlbumTableViewController.h
+//  Daoting
+//
+//  Created by Kevin on 14-5-12.
+//  Copyright (c) 2014年 赵 克鸣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AlbumTableViewController : UITableViewController
+
+@end
---
+++ Daoting/Daoting/SongTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SongTableViewController.h
+//  Daoting
+//
+//  Created by Kevin on 14-5-12.
+//  Copyright (c) 2014年 赵 克鸣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SongTableViewController : UITableViewController
+
+@end
